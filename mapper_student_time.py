6ddf41ea2f47417accf5fe1bb69d3296ba867a87
Sushant More <more.13@osu.edu>
2017-09-30 21:50:59 -0400

    skipping the first line

--- mapper_student_time.py
+++ mapper_student_time.py
@@ -7,6 +7,9 @@ reader = csv.reader(sys.stdin, delimiter='\t')
 
 for line in reader:
 
+    if line[0] == 'id':
+    	continue	
+
     author_id = line[3]
 
     time_added = line[8]
@@ -14,7 +17,5 @@ for line in reader:
     time_added_split = re.split('[" ":]', time_added)
     
     hour = time_added_split[1]
-    
-    print time_added_split
     
     print hour
